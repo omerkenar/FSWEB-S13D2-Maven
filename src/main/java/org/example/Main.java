dc36323eba960f1dfc06d91399867f1e04af5fbd
Ömer Kenar <omer.ken@hotmail.com>
2024-09-13 00:05:20 +0300

    2 ödev yapıldı

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,43 @@ public class Main {
 
 
     public static boolean isPalindrome(int sayi) {
-       return false;
+        int num = sayi;
+        int sayiPalindrome=0;
+        while (sayi != 0) {
 
+            int lastDigit = sayi % 10;
+            sayiPalindrome = sayiPalindrome * 10 + lastDigit;
+            sayi /= 10;
+        }
+        return num==sayiPalindrome;
     }
+
+    public static boolean isPerfectNumber(int sayi) {
+
+        if(sayi==1 && sayi<0)return false;
+
+        int perfectNumberToplam = 1;
+
+        for (int i =2; i<sayi; i++){
+            if (sayi%i==0 && sayi!=i){
+                perfectNumberToplam+=i;
+            }
+        };
+
+        if (perfectNumberToplam==sayi) return true;
+
+
+
+        return false;
+    }
+
+    public static String numberToWords(int rakam) {
+        return "true";
+    }
+
+
 }
+
+
+
+
